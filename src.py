1db00a986ab86800520c4d618cee6386eb8a925c
Nataly Sholohova <83698795+sholohova27@users.noreply.github.com>
2023-04-05 14:10:08 +0300

    Add files via upload

---
+++ src.py
@@ -0,0 +1,171 @@
+import json
+# эта ошибка возникала при пустом файле contacts.json, ментор посоветовал импортировать ее явно
+from json.decoder import JSONDecodeError
+
+
+# Загружаем словарь из файла или создаем пустой словарь (для сохранения данных)
+def read_contacts(file_name):
+    try:
+        with open(file_name, 'r') as f:
+            contacts = json.load(f)
+    except (FileNotFoundError, AttributeError, JSONDecodeError):
+        contacts = {}
+    return contacts
+
+
+# Записываем контакты в файл
+def save_contacts(file_name, contacts):
+    with open(file_name, 'w') as f:
+        if contacts:
+            json.dump(contacts, f)
+
+
+def Index_Key_error_func(func):
+    def inner(*args, **kwargs):
+        name = args[0].strip().lower()
+        contacts = kwargs['contacts']
+
+        try:
+            return func(*args, **kwargs)
+        except IndexError:
+            return f'Print name and phone number via space', contacts
+        except KeyError:
+            return f'Contact {name} is absent', contacts
+    return inner
+
+# contacts возвращаем для того, чтобы сигнатура ф-й была одинаковая,
+# kwargs['contacts']: 'contacts' это также ключ, по к-му можно найти в kwargs словарь contacts
+def hello_func(*args, **kwargs):
+    contacts = kwargs['contacts']
+    return "How can I help you?", contacts
+
+def help_func(*args, **kwargs):
+    contacts = kwargs['contacts']
+    return ''' 
+               For adding Contacts type "add"
+               To change Contacts type "change"
+               To get Contact`s phone number type "phone" and Contact`s name after
+               To get all Contacts "show all"
+               To delete Contact type "delete"
+               To exit type "bye"/"close"/"exit"/"." 
+            ''', contacts
+
+
+# передаем словарь Contacts из ф-и main в качестве аргумента
+@Index_Key_error_func
+def add_func(*args, **kwargs):
+    contacts = kwargs['contacts']
+    try:
+# Забираем первый и второй элемент, т.к. ф-я handler, которую вызываем в мейне,
+# возвращает ф-ю и очищенный от команды список, к-й распаковывается через * в
+# позиционные параметры add_funс (в мейне): result, contacts = func(*text, Contacts=Contacts)
+        name = args[0].strip().lower()
+        phone = args[1:]
+        phone = [phone.strip().lower() for phone in args[1:]]
+        if not contacts.get(name):
+            contacts[name] = phone
+        else:
+            return f'{name} already exists. Use "change" command to change number', contacts
+    except AttributeError:
+        contacts[name] = phone
+    return f"Contact {name} with phone {phone} successfully added", contacts
+
+@Index_Key_error_func
+def change_func(*args, **kwargs):
+    contacts = kwargs['contacts']
+# Забираем первый и второй элемент, т.к. ф-я handler, которую вызываем в мейне,
+# возвращает ф-ю и очищенный от команды список, к-й распаковывается через * в
+# позиционные параметры add_funс (в мейне): result = func(*text, Contacts=Contacts)
+    name = args[0].strip().lower()
+    old_phone = contacts[name]
+    contacts[name] = ""
+    new_phone = args[1:]
+    phone = [phone.strip().lower() for phone in args[1:]]
+    # если имени нет в словаре, оно добавится, если нет - поменяется номер
+    contacts[name] = new_phone
+    return f"Phone for contact {name} changed successfully.\nOld phone {old_phone}, new phone {new_phone}", contacts
+
+
+@Index_Key_error_func
+def del_func(*args, **kwargs):
+    contacts = kwargs['contacts']
+# Забираем первый и второй элемент, т.к. ф-я handler, которую вызываем в мейне,
+# возвращает ф-ю и очищенный от команды список, к-й распаковывается через * в
+# позиционные параметры add_funс (в мейне): result = func(*text, Contacts=Contacts)
+    name = args[0].strip().lower()
+    contacts.pop(name)
+    return f"Contact {name} successfully deleted", contacts
+
+@Index_Key_error_func
+def phone_func(*args, **kwargs):
+    contacts = kwargs['contacts']
+    name = args[0].strip().lower()
+    return str(contacts[name]), contacts
+
+
+
+def show_all_func(*args, **kwargs):
+    contacts = kwargs['contacts']
+    return '\n'.join([f'{name} : {phone}' for name, phone in contacts.items()]), \
+           contacts
+
+
+def unknown_command(*args, **kwargs):
+    contacts = kwargs['contacts']
+    return "Sorry, unknown command. Try again", contacts
+
+
+def exit_func(*args, **kwargs):
+    contacts = kwargs['contacts']
+    return "Bye", contacts
+
+# Ф-я handler проверяет, является ли введенный текст командой, сверяясь со словарем MODES,
+# и возвращает нужную ф-ю, а также текст после команды
+def handler(text):
+    for command, func in MODES.items():
+        if text.lower().startswith(command):
+            return func, text.replace(command,'').strip().split()
+    # else тут нельзя, он вернет только 1-ю ф-ю словаря MODES, если ей соответствует введенная
+    # команда, но следующей ф-ции она уже соответствовать не будет, поэтому вернет unknown_command для всех остальных
+    return unknown_command, []
+# у ретернов должна быть одинаковая структура, поэтому после 2-го возвращаем [] (None приводит к ошибке, потому что его нельзя распаковать *),
+# и обязательно добавляем 2-ю переменную в ф-и Main (func, text = handler(input('>>>'))), т.к. handler возвращает 2
+# и теперь нужно добавить в каждую ф-цию параметр *args, потому что в ф-ции теперь нужно передавать этот параметр тоже
+
+
+# Создаем словарь MODES из всех промежуточных ф-ций (каррирование)
+MODES = {"hello": hello_func,
+         "add": add_func,
+         "change": change_func,
+         "help": help_func,
+         "delete": del_func,
+         "phone": phone_func,
+         "show all": show_all_func,
+         "close": exit_func,
+         "exit": exit_func,
+         "bye": exit_func,
+         ".": exit_func}
+
+
+# Передаем имя файла и путь к файлу с контактами в качестве аргументов
+def main(file_name):
+    contacts = read_contacts(file_name)
+    while True:
+        # Ф-я handler проверяет, является ли введенный текст командой, сверяясь со словарем MODES,
+        # и возвращает нужную ф-ю, а также список из текста после команды
+        func, text = handler(input('>>>'))
+        # можно просто result, но так легче масштабировать, перезаписывая в contacts
+        # вместо исходного словаря результат выполнения ф-ций
+        result, contacts = func(*text, contacts=contacts)
+        print(result)
+        if func == exit_func:
+            save_contacts(file_name, contacts)
+            break
+
+
+
+
+# Проверяем, что скрипт запущен как основной
+if __name__ == '__main__':
+    file_name = 'contacts.json'
+    main(file_name)
